bdeed3d4af65562f9df050376ade310057492156
Harrydmyh <harrydm@bu.edu>
2025-09-24 22:09:16 -0400

    Finished basic html functions for mini_insta

--- mini_insta/models.py
+++ mini_insta/models.py
@@ -12,7 +12,7 @@ class Profile(models.Model):
     display_name = models.TextField(blank=True)
     profile_image_url = models.URLField(blank=True)
     bio_text = models.TextField(blank=True)
-    join_date = models.DateTimeField(auto_now=True)
+    join_date = models.DateField(blank=True)
 
     def __str__(self) -> str:
         """return a string representation of this model instance"""
--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -2,5 +2,9 @@
 # urls for the mini-insta application
 
 from django.urls import path
+from .views import ProfileListView, ProfileDetailView
 
-urlpatterns = []
+urlpatterns = [
+    path("", ProfileListView.as_view(), name="show_all_profiles"),
+    path("profile/<int:pk>", ProfileDetailView.as_view(), name="show_profile"),
+]
