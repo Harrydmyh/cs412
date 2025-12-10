14d29ddc788a5b88b2b4f267e8944f0141efcf6b
Harrydmyh <harrydm@bu.edu>
2025-12-09 20:47:27 -0500

    Fixed bug

--- attendance/views.py
+++ attendance/views.py
@@ -21,7 +21,7 @@ import csv
 class RoleRequiredMixin(LoginRequiredMixin):
     """A parent mixin for custom user requirements"""
 
-    login_url = "/attendance/login"
+    login_url = "/harrydm/attendance/login"
     required_role = None  # "instructor", "student", or None for any user
 
     def handle_no_permission(self):
